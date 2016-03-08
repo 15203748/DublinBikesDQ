4c8a508483e1cd8aa30d7f9eedd50f52b1e0a8b2
John Windsor <john.windsor@ucdconnect.ie>
2016-03-08 18:40:49 +0000

    Addition of ADO.NET

---
+++ Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.cmdStart = New System.Windows.Forms.Button()
+        Me.cmdTerminate = New System.Windows.Forms.Button()
+        Me.tmrPoll = New System.Windows.Forms.Timer(Me.components)
+        Me.lstLog = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'cmdStart
+        '
+        Me.cmdStart.Location = New System.Drawing.Point(21, 143)
+        Me.cmdStart.Name = "cmdStart"
+        Me.cmdStart.Size = New System.Drawing.Size(156, 23)
+        Me.cmdStart.TabIndex = 0
+        Me.cmdStart.Text = "Start DQ Listener"
+        Me.cmdStart.UseVisualStyleBackColor = True
+        '
+        'cmdTerminate
+        '
+        Me.cmdTerminate.Location = New System.Drawing.Point(249, 143)
+        Me.cmdTerminate.Name = "cmdTerminate"
+        Me.cmdTerminate.Size = New System.Drawing.Size(156, 23)
+        Me.cmdTerminate.TabIndex = 1
+        Me.cmdTerminate.Text = "Terminate DQ"
+        Me.cmdTerminate.UseVisualStyleBackColor = True
+        '
+        'tmrPoll
+        '
+        Me.tmrPoll.Interval = 10000
+        '
+        'lstLog
+        '
+        Me.lstLog.FormattingEnabled = True
+        Me.lstLog.ItemHeight = 17
+        Me.lstLog.Location = New System.Drawing.Point(21, 11)
+        Me.lstLog.Name = "lstLog"
+        Me.lstLog.ScrollAlwaysVisible = True
+        Me.lstLog.Size = New System.Drawing.Size(384, 123)
+        Me.lstLog.TabIndex = 2
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(427, 176)
+        Me.Controls.Add(Me.lstLog)
+        Me.Controls.Add(Me.cmdTerminate)
+        Me.Controls.Add(Me.cmdStart)
+        Me.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Dublin Bikes Data Queue Listener..."
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents cmdStart As Button
+    Friend WithEvents cmdTerminate As Button
+    Friend WithEvents tmrPoll As Timer
+    Friend WithEvents lstLog As ListBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,88 @@
+﻿Public Class frmMain
+
+    Private cnnDB As New ADODB.Connection
+    Private RS As New ADODB.Recordset
+    Private strSQL = "SELECT * FROM [DublinBikes_SourceData] ORDER BY [DB_ID] ASC;"
+    Private strCSVLive = "C:\Users\winds\Documents\UCD Semester 02\COMP30670 - Software Engineering\Dublin Bikes\DublinBikes\CSV Files\"
+    Private strCSVArchive = "C:\Users\winds\Documents\UCD Semester 02\COMP30670 - Software Engineering\Dublin Bikes\DublinBikes\CSV Files\Archive\"
+
+    Private Sub DoPoll()
+        Dim strFile As String
+        strFile = Dir(strCSVLive)
+        If strFile = "" Then
+            Exit Sub
+        Else
+            strFile = strCSVLive & strFile
+
+            Using MyReader As New Microsoft.VisualBasic.FileIO.TextFieldParser(strFile)
+                MyReader.TextFieldType = FileIO.FieldType.Delimited
+                MyReader.SetDelimiters(",")
+                Dim currentRow As String()
+                currentRow = MyReader.ReadFields()
+                While Not MyReader.EndOfData
+                    Try
+                        currentRow = MyReader.ReadFields()
+                        Dim currentField As String
+                        For Each currentField In currentRow
+                            MsgBox(currentField)
+                        Next
+                    Catch ex As Microsoft.VisualBasic.FileIO.MalformedLineException
+                        MsgBox("Line " & ex.Message & "is not valid and will be skipped.")
+                    End Try
+                End While
+            End Using
+
+            RS.CursorLocation = ADODB.CursorLocationEnum.adUseServer
+            RS.CursorType = ADODB.CursorTypeEnum.adOpenDynamic
+            RS.LockType = ADODB.LockTypeEnum.adLockOptimistic
+            RS.Open(strSQL, cnnDB,)
+            AddToList("Data Recordset Open...")
+            AddToList(Format(RS.RecordCount, "#,##0") & " Records currently in the recordset...")
+            RS.Close()
+            AddToList("Data Recordset Closed...")
+        End If
+
+    End Sub
+
+    Private Sub AddToList(strDown As String)
+        lstLog.Items.Add("")
+        lstLog.Items.Add(strDown)
+        lstLog.SelectedIndex = (lstLog.Items.Count - 1)
+
+    End Sub
+
+    Private Sub cmdStart_Click(sender As Object, e As EventArgs) Handles cmdStart.Click
+        Dim strSQL As String
+        AddToList("Establishing Connection to Database...")
+        Try
+            cnnDB.Open("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\winds\Documents\UCD Semester 02\COMP30670 - Software Engineering\Dublin Bikes\Dublin Bikes.accdb;Persist Security Info=False;")
+        Catch ex As Exception
+            MsgBox(ex.Message, vbOKOnly, "Error...")
+        End Try
+        AddToList("Database Connection Established...")
+        tmrPoll.Enabled = True
+    End Sub
+
+    Private Sub cmdTerminate_Click(sender As Object, e As EventArgs) Handles cmdTerminate.Click
+        tmrPoll.Enabled = False
+        AddToList("Terminating Connection to Database...")
+        Try
+            cnnDB.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message, vbOKOnly, "Error...")
+        End Try
+        AddToList("Database Connection Terminated...")
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        AddToList("DQ Listener Initialised...")
+    End Sub
+
+    Private Sub tmrPoll_Tick(sender As Object, e As EventArgs) Handles tmrPoll.Tick
+        Try
+            Call DoPoll()
+        Catch ex As Exception
+            MsgBox(ex.Message, vbOKOnly, "Error...")
+        End Try
+    End Sub
+End Class
